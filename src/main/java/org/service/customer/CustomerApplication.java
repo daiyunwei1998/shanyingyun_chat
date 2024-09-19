d0187e770350f1ffab8fc1411dbcd589398c11bf
Yunwei Dai <daiyunwei1998@gmail.com>
2024-09-19 10:37:36 +0800

    fix: fix application configuration to remove exclusion of DataSourceAutoConfiguration

--- src/main/java/org/service/customer/CustomerApplication.java
+++ src/main/java/org/service/customer/CustomerApplication.java
@@ -2,9 +2,8 @@ package org.service.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class CustomerApplication {
 
     public static void main(String[] args) {
