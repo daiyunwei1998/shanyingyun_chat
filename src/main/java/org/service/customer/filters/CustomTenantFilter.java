bca60a38f936c5c63046cb5fc79b3f7d287269cd
Yunwei Dai <daiyunwei1998@gmail.com>
2024-09-19 18:29:21 +0800

    refactor: Update CustomTenantFilter to read tenantId
    This commit updates the CustomTenantFilter class to include a new method, parseRequestBody, which parses the request body as JSON using the Jackson ObjectMapper. This method is used to extract the JSON payload from the request and returns a JsonNode object. This change improves the functionality of the filter by allowing it to handle JSON request bodies.
    
    Refactor the CustomTenantFilter class to include the parseRequestBody method for JSON parsing.

--- src/main/java/org/service/customer/filters/CustomTenantFilter.java
+++ src/main/java/org/service/customer/filters/CustomTenantFilter.java
@@ -1,23 +1,29 @@
 package org.service.customer.filters;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.service.customer.service.CustomUserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class CustomTenantFilter extends OncePerRequestFilter {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String tenantId = request.getHeader("X-Tenant-ID"); // Assume tenantId comes from a custom header
-
+        log.error(tenantId);
         if (tenantId != null) {
             CustomUserDetailsService.setTenantContext(tenantId);
         }
@@ -26,6 +32,13 @@ public class CustomTenantFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
         } finally {
             CustomUserDetailsService.clearTenantContext();
+        }
+    }
+    private JsonNode parseRequestBody(HttpServletRequest request) {
+        try {
+            return objectMapper.readTree(request.getInputStream());
+        } catch (IOException e) {
+            return null;
         }
     }
 }
