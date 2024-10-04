083f60e3e645a0f2d90cb75775388c57a3f0566c
Yunwei Dai <daiyunwei1998@gmail.com>
2024-10-04 13:29:23 +0800

    Refactor UserInfo DTO to include a no-args constructor

--- src/main/java/org/service/customer/dto/user/UserInfo.java
+++ src/main/java/org/service/customer/dto/user/UserInfo.java
@@ -3,9 +3,11 @@ package org.service.customer.dto.user;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserInfo {
     @JsonProperty("user_id")
     private String userId;
